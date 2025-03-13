eebf4dc7a7ee947902dd5962b7bc7ea8dc534998
Ahmed-Raza-Rehmani <ahmedrazarehmani2345678@gmail.com>
2025-03-13 22:09:02 +0500

    Change Provider Setting for better architecture

--- lib/add_to_cart_page.dart
+++ lib/add_to_cart_page.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shoes_shopping_app/cart_provider.dart';
 import 'package:shoes_shopping_app/global_variables.dart';
 
 class AddToCartPage extends StatelessWidget {
   const AddToCartPage({super.key});
   @override
   Widget build(BuildContext context) {
+    final cartListD = context.watch<CartProvider>().productL;
     return ListView.builder(
-      itemCount: cart.length,
+      itemCount: cartListD.length,
       itemBuilder: (context, index) {
+        final cartItem = cartListD[index];
         return ListTile(
-          title: Text(cart['title'].toString()),
+          title: Text(cartItem['title'].toString()),
 
-          subtitle: Text(cart['size'].toString()),
+          subtitle: Text(cartItem['size'].toString()),
           leading: CircleAvatar(
-            backgroundImage: AssetImage(cart['imageUrl'].toString()),
+            radius: 30,
+            backgroundImage: AssetImage(cartItem['imageUrl'].toString()),
           ),
         );
       },
--- lib/cart_provider.dart
+++ lib/cart_provider.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class CartProvider extends ChangeNotifier {
   final List<Map<String, dynamic>> productL = [];
 
-  void addProduct(Map<String, dynamic> productV) {
-    productL.add(productV);
+  void addProduct(Map<String, dynamic> productM) {
+    productL.add(productM);
     notifyListeners();
   }
 
-  void removeProduct(Map<String, dynamic> productV) {
-    productL.remove(productV);
+  void removeProduct(Map<String, dynamic> productM) {
+    productL.remove(productM);
     notifyListeners();
   }
 }
